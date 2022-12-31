7bd4c588c88e5e2131162b7d3b2274e831c313f0
scriptkiddieke <alexmwangimungai254@gmail.com>
2023-01-01 00:00:00 +0300

    Hack the contribution graph 1

--- git.py
+++ git.py
@@ -5,9 +5,10 @@ from datetime import datetime, timedelta
 # Configure these variables
 REPO_PATH = r'C:\Users\alexm\Desktop\Projects\python'  # Using raw string for Windows path
 COMMIT_MESSAGE = 'Hack the contribution graph'
-START_DATE = '2024-01-01'
-DAYS = 170
-TEMP_FILE_NAME = 'temp.txt'
+START_DATE = '2023-01-01'
+DAYS = 20
+DAYS_TO_SKIP = 2  # Number of days to skip after each commit
+TEMP_FILE_NAME = 'temp.py'
 
 def run_command(command, env=None):
     """Run a shell command and return the output."""
@@ -31,12 +32,14 @@ def main():
     os.chdir(REPO_PATH)
     
     start_date = datetime.strptime(START_DATE, '%Y-%m-%d')
+    commit_counter = 1
     
     for i in range(DAYS):
-        commit_date = start_date + timedelta(days=i)
+        commit_date = start_date + timedelta(days=i * (DAYS_TO_SKIP + 1))  # Adjust the date to skip days
         commit_date_str = commit_date.strftime('%Y-%m-%dT%H:%M:%S')
         print(f"Creating commit for {commit_date_str}")
-        change_date_and_commit(commit_date_str, i + 1)
+        change_date_and_commit(commit_date_str, commit_counter)
+        commit_counter += 1
     
     # Push the changes
     run_command('git push origin main')
