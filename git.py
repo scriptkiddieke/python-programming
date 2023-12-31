9f0b6b0034de2ba844b664a255c83a3a989553ab
scriptkiddieke <alexmwangimungai254@gmail.com>
2024-01-01 00:00:00 +0300

    Hack the contribution graph 1

---
+++ git.py
@@ -0,0 +1,45 @@
+import os
+import subprocess
+from datetime import datetime, timedelta
+
+# Configure these variables
+REPO_PATH = r'C:\Users\alexm\Desktop\Projects\python'  # Using raw string for Windows path
+COMMIT_MESSAGE = 'Hack the contribution graph'
+START_DATE = '2024-01-01'
+DAYS = 170
+TEMP_FILE_NAME = 'temp.txt'
+
+def run_command(command, env=None):
+    """Run a shell command and return the output."""
+    result = subprocess.run(command, shell=True, capture_output=True, text=True, env=env)
+    return result.stdout.strip()
+
+def change_date_and_commit(date, counter):
+    """Change the date, make a change in a file, and make a commit."""
+    env = os.environ.copy()
+    env['GIT_AUTHOR_DATE'] = date
+    env['GIT_COMMITTER_DATE'] = date
+    
+    with open(TEMP_FILE_NAME, 'a') as temp_file:
+        temp_file.write(f'Commit number {counter}\n')
+    
+    run_command('git add .')
+    run_command(f'git commit -m "{COMMIT_MESSAGE} {counter}"', env=env)
+
+def main():
+    # Ensure we're in the correct directory
+    os.chdir(REPO_PATH)
+    
+    start_date = datetime.strptime(START_DATE, '%Y-%m-%d')
+    
+    for i in range(DAYS):
+        commit_date = start_date + timedelta(days=i)
+        commit_date_str = commit_date.strftime('%Y-%m-%dT%H:%M:%S')
+        print(f"Creating commit for {commit_date_str}")
+        change_date_and_commit(commit_date_str, i + 1)
+    
+    # Push the changes
+    run_command('git push origin main')
+
+if __name__ == "__main__":
+    main()
