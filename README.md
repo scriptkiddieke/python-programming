2e7187f61296f1b61dabcfed3e950fe0be8af89e
scriptkiddieke <alexmwangimungai254@gmail.com>
2024-07-17 08:49:27 +0300

    Initial commit

--- README.md
+++
@@ -1,5 +0,0 @@
-<<<<<<< HEAD
-# python-programming.
-=======
-# python-programming
->>>>>>> d0b042e95924f560652e5bea4a0a4a4a37944c38
---
+++ m-pesa.py
@@ -0,0 +1,18 @@
+# --------------------  A PYTHON PROGRAM T GENERATE A HOW M-PESA SYSTEM WORKD ----------------------------- #
+print("""
+      Welcome to Safaricom Network
+      1. Safaricom+ 
+      2. M-Pesa
+      """) 
+     
+    # select an option
+option = int(input("Select an option: "))
+
+if option == 1:
+    print("Welcome to saraicom network")
+
+elif option == 2:
+    print("Welcome to Mpesa network")
+
+
+    
